a55d87df604db96221b0179a95eeb5d69b83ab4a
jerome <jeromebrown97@gmail.com>
2021-07-08 22:49:40 -0500

    inital commit for CarDealership API with EF

---
+++ CarDealershipAPI/Models/CarDealershipContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace CarDealershipAPI.Models
+{
+    public partial class CarDealershipContext : DbContext
+    {
+        public CarDealershipContext()
+        {
+        }
+
+        public CarDealershipContext(DbContextOptions<CarDealershipContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Car> Cars { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=.\\MSSQLSERVER01;Database=CarDealership;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Car>(entity =>
+            {
+                entity.ToTable("Car");
+
+                entity.Property(e => e.Color).HasMaxLength(15);
+
+                entity.Property(e => e.Make)
+                    .IsRequired()
+                    .HasMaxLength(25);
+
+                entity.Property(e => e.Model)
+                    .IsRequired()
+                    .HasMaxLength(25);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
